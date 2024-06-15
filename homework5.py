a43e8ae304a32cd3d28c2ed7784d82fac962a6c4
GeorgyDzhanaev <georgy.dzhanaev@yandex.ru>
2024-06-15 22:27:38 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,14 @@
+immutable_var = tuple
+tuple =  1,2,"George",True
+print("Immutable tuple:",tuple)
+#tuple_2 = 1,2,4,"apple",False
+#print(tuple_2)
+#tuple_2 [0] = 300
+#print(tuple_2)
+mutable_list = tuple
+tuple_3 = ([1,10,"table", "chut chut ne ponyatno"])
+tuple_3[3] = 'teper chut ponyatney'
+print("Mutable_list:",tuple_3)
+mutable_list = ([1,10,"table", "chut chut ne ponyatno"])
+mutable_list[3] = 'teper chut ponyatney'
+print("Mutable_list:",mutable_list)
